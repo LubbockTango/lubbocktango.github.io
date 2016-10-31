e8428b3a63dace9031a9245dbe05ca1c12577bdc
Hannah <hannahmestes@gmail.com>
2016-10-31 13:35:37 -0500

    added carousel and hover effect

--- init.js
+++ init.js
@@ -1,7 +1,11 @@
 (function($){
-  $(function(){
+  $(document).ready(function(){
 
     $('.button-collapse').sideNav();
+    //$('.carousel').carousel({full_width; true});
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({full_width: true});
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
